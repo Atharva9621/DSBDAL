33669bf684dd8cc58014aa4dc047c118e0be75c9
Atharva <atharvad931@gmail.com>
2024-04-30 09:19:08 +0530

    update dir structure

---
+++ Assignment13/wordcount.scala
@@ -0,0 +1,5 @@
+val data = sc.textFile("/home/pict/Desktop/spark.txt")
+val splitData = data.flatMap(line => line.split(" "))
+val mapData = splitData.map(word => (word, 1))
+val reduceData = mapData.reduceByKey((+))
+reduceData.collect
