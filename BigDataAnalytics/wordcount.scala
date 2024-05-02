8fe6d56dcd73da0ff4ebed271c41d7573bf833b1
Atharva Date <100025827+Atharva9621@users.noreply.github.com>
2024-05-02 07:19:56 +0530

    Update wordcount.scala

--- BigDataAnalytics/wordcount.scala
+++ BigDataAnalytics/wordcount.scala
@@ -1,5 +1,5 @@
 val data = sc.textFile("/home/pict/Desktop/spark.txt")
 val splitData = data.flatMap(line => line.split(" "))
 val mapData = splitData.map(word => (word, 1))
-val reduceData = mapData.reduceByKey((+))
+val reduceData = mapData.reduceByKey((_+_))
 reduceData.collect
